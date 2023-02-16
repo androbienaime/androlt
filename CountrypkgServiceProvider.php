963fe93717bafa3d854299e229f2d46ca5a59024
Lestruviens <admin@lestruviens.com>
2023-02-15 22:51:28 -0500

    ..

---
+++ CountrypkgServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AndroLT\Countrypkg;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ *
+ */
+class CountrypkgServiceProvider extends ServiceProvider
+{
+
+	public function boot()
+	{
+		$this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+	}
+
+	public function register()
+	{
+		$this->commands([
+			Console\Commands\GenerateCountries::class,
+			Console\Commands\GenerateStates::class,
+			Console\Commands\GenerateCity::class,
+		]);
+	}
+}
